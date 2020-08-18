690bc41f4ee7442e662b9fc0f21adeaa059d824a
Cameron <influence365@gmail.com>
2020-08-18 14:49:14 +0100

    update to sortingVisualizer.js

--- src/SorterVisualizer/SorterVisualizer.js
+++ src/SorterVisualizer/SorterVisualizer.js
@@ -34,6 +34,7 @@ class SorterVisualizer extends Component {
     }
 
     runSort = () => {
+        console.log(`Running ${this.state.sortMethod} Sort Method`)
         switch (this.state.sortMethod) {
             case 'bubble':
                 this.runBubbleSort();
@@ -44,27 +45,50 @@ class SorterVisualizer extends Component {
             case 'quickSort':
                 this.runQuickSort();
                 break;
+            case 'heapSort':
+                this.runHeapSort();
             default:
                 break;
         }
     }
 
-    runBubbleSort() {
-        console.log(this.state.array);
-        let array = this.state.array;
-        //let extraClass = this.state.extraClass;
-        let a = bubbleSortAnimations(array)
-
-        for (let i = 0; i < a.length - 1; i += 2) {
-
+    runHeapSort() {
+        const trace = HeapSort(this.state.array)
+        console.log(trace)
+        for (let i = 0; i < trace.length; i++) {
+            // console.log(item)
             setTimeout(() => {
-                array[a[i][0]] = a[i][1];
-                array[a[i + 1][0]] = a[i + 1][1];
-                //extraClass[a[i+1]] = 'isActive';
-                this.setState({ arrayAccess: i })
-                this.setState({ array });
+                this.setState({ array: trace[i].array })
+            }, i * 10)
+        }
+    }
 
-            }, i * ANIMATION_SPEED_MS);
+    runBubbleSort() {
+        // console.log(this.state.array);
+        // let array = this.state.array;
+        // //let extraClass = this.state.extraClass;
+        // let a = bubbleSortAnimations(array)
+        // console.log(a)
+        // for (let i = 0; i < a.length - 1; i += 2) {
+
+        //     setTimeout(() => {
+        //         array[a[i][0]] = a[i][1];
+        //         array[a[i + 1][0]] = a[i + 1][1];
+        //         //extraClass[a[i+1]] = 'isActive';
+        //         this.setState({ arrayAccess: i })
+        //         this.setState({ array });
+
+        //     }, i * ANIMATION_SPEED_MS);
+        // }
+
+        const trace = BubbleSort(this.state.array)
+        console.log(trace)
+
+        for (let i = 0; i < trace.length; i++) {
+            // console.log(item)
+            setTimeout(() => {
+                this.setState({ array: trace[i].array })
+            }, i * 10)
         }
     }
 
@@ -114,6 +138,7 @@ class SorterVisualizer extends Component {
                             onChange={this.handleChange} >
                             <option value="bubble">Bubble Sort</option>
                             <option value="insertion">Insertion Sort</option>
+                            <option value="heapSort">Heap Sort</option>
                         </select>
 
                     </span>
@@ -156,6 +181,75 @@ const colorMap = (percent, start, end) => {
     return `hsl(${c}, 100%, 50%)`
 
 }
+
+const swap = (array, i, j) => {
+    const tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
+}
+
+const newTrace = (array) => {
+    return [
+        {
+            array: [...array],
+            groupA: [],
+            groupB: [],
+            groupC: [],
+            groupD: [],
+            sortedIndices: []
+        }
+    ];
+};
+
+
+const addToTrace = (
+    trace,
+    array,
+    sortedIndices = [],
+    groupA = [],
+    groupB = [],
+    groupC = [],
+    groupD = []
+) => {
+    trace.push({
+        array: [...array],
+        groupA: [...groupA],
+        groupB: [...groupB],
+        groupC: [...groupC],
+        groupD: [...groupD],
+        sortedIndices: [...sortedIndices]
+    });
+};
+
+const lastSorted = (trace) => {
+    return trace[trace.length - 1].sortedIndices;
+};
+
+const BubbleSort = (nums) => {
+    // Set up code for tracing the algorithm
+    const trace = newTrace(nums);
+
+    // Sorting Algorithm with trace capture
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length - i - 1; j++) {
+            // Visualize: Comparing A[j] and A[j + 1]
+            addToTrace(trace, nums, lastSorted(trace), [j, j + 1]);
+            if (nums[j] > nums[j + 1]) {
+                swap(nums, j, j + 1);
+                // Visualize: Swap A[j] and A[j + 1]
+                addToTrace(trace, nums, lastSorted(trace), [], [j, j + 1]);
+            }
+        }
+
+        // Visualize: final value is sorted
+        addToTrace(trace, nums, [
+            ...lastSorted(trace),
+            nums.length - 1 - i
+        ]);
+    }
+
+    return trace;
+};
 
 const setInitialValues = () => {
     const row = [];
@@ -250,3 +344,98 @@ function randomIntFromInterval(min, max) {
 
 export default SorterVisualizer;
 
+const createRange = (start, end) => {
+    return [...Array(end - start).keys()].map((elem) => elem + start);
+};
+
+const HeapSort = (nums) => {
+    const trace = newTrace(nums);
+
+    // Helper functions to quickly access nodes
+    const left = (i) => 2 * i + 1;
+    const right = (i) => 2 * i + 2;
+    const parent = (i) => Math.floor((i - 1) / 2);
+
+    const maxHeapify = (array, i, heapsize) => {
+        const leftChild = left(i);
+        const rightChild = right(i);
+
+        // Visualize: Compare parent and leftChild
+        addToTrace(trace, array, lastSorted(trace), [i, leftChild]);
+
+        let largest =
+            leftChild < heapsize && array[leftChild] > array[i]
+                ? leftChild
+                : i;
+
+        // Visualize: Compare largest and rightChild
+        addToTrace(trace, array, lastSorted(trace), [largest, rightChild]);
+
+        if (rightChild < heapsize && array[rightChild] > array[largest])
+            largest = rightChild;
+
+        if (largest !== i) {
+            // Visualize: Select largest child and parent
+            addToTrace(trace, array, lastSorted(trace), [], [i, largest]);
+
+            swap(array, i, largest);
+
+            // Visualize: Swap largest child and parent
+            addToTrace(trace, array, lastSorted(trace), [], [i, largest]);
+
+            maxHeapify(array, largest, heapsize);
+        }
+    };
+
+    const BuildMaxHeap = (array) => {
+        const start = Math.floor(array.length / 2);
+        const heapsize = array.length;
+        for (let i = start; i >= 0; i--) {
+            maxHeapify(array, i, heapsize);
+        }
+
+        // Visualize: Mark heap as built
+        addToTrace(
+            trace,
+            array,
+            lastSorted(trace),
+            [],
+            [],
+            [],
+            createRange(0, array.length)
+        );
+    };
+
+    const heapSort = (array) => {
+        BuildMaxHeap(array);
+        let heapsize = array.length;
+        for (let i = array.length - 1; i > 0; i--) {
+            // Visualize: Select Maximum
+            addToTrace(trace, array, lastSorted(trace), [], [0, i]);
+
+            swap(array, 0, i);
+            heapsize -= 1;
+
+            // Visualize: Swap with last element in heap
+            addToTrace(trace, array, [...lastSorted(trace), i], [], [0, i]);
+
+            maxHeapify(array, 0, heapsize);
+
+            // Visualize: Heap created
+            addToTrace(
+                trace,
+                array,
+                lastSorted(trace),
+                [],
+                [],
+                [],
+                createRange(0, heapsize)
+            );
+        }
+        addToTrace(trace, array, [...lastSorted(trace), 0]);
+    };
+
+    // Execute Heapsort
+    heapSort(nums);
+    return trace;
+};
